8e789fae124415881edeb9e5c6173c1da3c08341
devcfgc <gomezcarlosfernando@hotmail.com>
2018-12-15 06:30:40 +0100

    Create entities project

---
+++ NorthWind.Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace NorthWind.Entities
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
