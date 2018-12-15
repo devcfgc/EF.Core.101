329fb4e17bd2aff8d44a0936749e7ed2927b8618
devcfgc <gomezcarlosfernando@hotmail.com>
2018-12-15 06:50:10 +0100

    Configure EF core. Add migrations.

---
+++ NorthWind.DAL/NorthWindContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthWind.Entities;
+
+namespace NorthWind.DAL
+{
+    public class NorthWindContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\MSSQLLocalDB;Database=NorthWind;Trusted_Connection=True"
+            );
+        }
+    }
+}
