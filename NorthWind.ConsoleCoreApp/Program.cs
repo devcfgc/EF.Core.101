0567127abace05f6f7fd85f799ebf81cba9b7176
devcfgc <gomezcarlosfernando@hotmail.com>
2018-12-20 05:48:17 +0100

    Add ConsoleCoreApp

---
+++ NorthWind.ConsoleCoreApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NorthWind.DAL;
+using NorthWind.Entities;
+
+namespace NorthWind.ConsoleCoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new NorthWindContext())
+            {
+                Console.WriteLine("Category name:");
+                var categoryName = Console.ReadLine();
+                var categoryNew = new Category
+                {
+                    CategoryName = categoryName
+                };
+                context.Categories.Add(categoryNew);
+                context.SaveChanges();
+
+                Console.WriteLine("Categories:");
+                foreach (var category in context.Categories)
+                {
+                    Console.WriteLine($"{category.CategoryId}, {category.CategoryName}");
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
