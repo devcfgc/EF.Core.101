97a4b7b5111896b1e377eb2efc72b4b9bfc1eac3
devcfgc <gomezcarlosfernando@hotmail.com>
2018-12-27 06:22:34 +0100

    Configure NorthWind.DAL.SQLite.Xamarin

---
+++ NorthWind.DAL.SQLite.Xamarin/NorthWindContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using NorthWind.Entities;
+
+namespace NorthWind.DAL.SQLite.Xamarin
+{
+    public class NorthWindContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        private string _dbPath = "";
+
+        public NorthWindContext(string dbPath)
+        {
+            _dbPath = dbPath;
+        }
+
+        public NorthWindContext() : this("NorthWind.db") {}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={_dbPath}");
+        }
+    }
+}
