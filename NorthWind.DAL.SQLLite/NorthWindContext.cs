1d030cebdb732e7e8b07e847f66b13d9dfd999eb
devcfgc <gomezcarlosfernando@hotmail.com>
2018-12-27 05:51:11 +0100

    Add UWP project with EF and sqlite

---
+++ NorthWind.DAL.SQLLite/NorthWindContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthWind.Entities;
+
+namespace NorthWind.DAL.SQLLite
+{
+    public class NorthWindContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=NorthWind.db");
+        }
+    }
+}
---
+++ NorthWind.UWPMigrations/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NorthWind.UWPMigrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //THE ONLY PURPOSE OF THIS PROJECT IS TO HELP US CREATING MIGRATIONS FOR:
+            // Northwind.UWPCpreApp
+            // NorthWind.DAL.SQLlite
+            // It needs a references to NorthWind.DAL.SQLlite and NorthWind.Entitites
+            // Install Microsoft.EntityFrameworkCore.Tools
+            // Add-Migration InitialCreate -StartupProject NorthWind.UWPMigrations
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
